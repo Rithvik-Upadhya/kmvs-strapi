b74a3eef10ae795564abf73ffe41aab4acb1b9a7
Rithvik <x394@protonmail.com>
2023-07-28 15:43:20 +0530

    Added Netlify integration

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = ({ env }) => ({
   graphql: {
     config: {
       endpoint: "/graphql",
@@ -11,4 +11,19 @@ module.exports = {
       },
     },
   },
-};
+  "netlify-deployments": {
+    enabled: true,
+    config: {
+      accessToken: process.env.NETLIFY_ACCESS_TOKEN,
+      sites: [
+        {
+          name: "kmvs",
+          id: "933f8fcc-7ea1-41a1-bdf2-f78dce392672",
+          buildHook:
+            "https://api.netlify.com/build_hooks/64c38792e3215d4eeaa02434",
+          branch: "main",
+        },
+      ],
+    },
+  },
+});
